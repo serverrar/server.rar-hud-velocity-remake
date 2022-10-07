3226bf00ef32d11ccc01cdf33454797d298cc96d
server.rar <107357628+serverrar@users.noreply.github.com>
2022-10-07 09:26:13 +0200

    Add files via upload

---
+++ S-Tempui Velocity Remake/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "adamant"
+
+game "gta5"
+
+client_script "client.lua"
+
+server_script "server.lua"
+
+ui_page 'html/ui.html'
+
+files {
+	'html/ui.html',
+	'html/css/app.css',
+	'html/css/grid.css',
+	'html/js/main.js',
+	'html/img/*',
+	'html/fonts/*'
+}
+
